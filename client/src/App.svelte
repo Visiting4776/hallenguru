2a5a1674f93480ff52ceef08c1e95e42cf8bb43a
mahathu <m.hoffmann@systemli.org>
2022-02-25 17:42:38 +0100

    some frontend styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,11 +2,75 @@
 	import { onMount } from 'svelte';
 	import Event from "./components/Event.svelte";
 	
-	let events = [];
-	onMount(async () => {
-		const res = await fetch("./get_bath_info");
-		events = await res.json();
-	});
+	let events = [
+  {
+    "bath_id": 1, 
+    "date": "Fri, 25 Feb 2022 00:00:00 GMT", 
+    "end_time": "19:45", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "17:45", 
+    "state": "reserved"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Fri, 25 Feb 2022 00:00:00 GMT", 
+    "end_time": "22:00", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "20:00", 
+    "state": "soldout"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Sat, 26 Feb 2022 00:00:00 GMT", 
+    "end_time": "08:30", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "06:30", 
+    "state": "soldout"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Sat, 26 Feb 2022 00:00:00 GMT", 
+    "end_time": "10:45", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "08:45", 
+    "state": "soldout"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Sat, 26 Feb 2022 00:00:00 GMT", 
+    "end_time": "13:00", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "11:00", 
+    "state": "soldout"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Sat, 26 Feb 2022 00:00:00 GMT", 
+    "end_time": "15:15", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "13:15", 
+    "state": "soldout"
+  }, 
+  {
+    "bath_id": 1, 
+    "date": "Sat, 26 Feb 2022 00:00:00 GMT", 
+    "end_time": "17:30", 
+    "from_date": "", 
+    "name": "Stadtbad Mitte", 
+    "start_time": "15:30", 
+    "state": "soldout"
+  },
+];
+	// onMount(async () => {
+	// 	const res = await fetch("./get_bath_info");
+	// 	events = await res.json();
+	// });
 	// function getBaths() {
 	// 	fetch("./get_bath_info")
 	// 		.then(d => d.text())
@@ -14,10 +78,17 @@
 	// }
 </script>
 
-<main>
-	<h1>Hello!</h1>
-
+<div class="mt-2 mx-auto max-w-xl bg-white shadow-md px-3 py-5">
+<h1>Today</h1>
+<div class="space-y-3 divide-y divide-solid">
 	{#each events as event}
 		<Event {event}/>
 	{/each}
-</main>
+</div>
+</div>
+
+<style global lang="postcss">
+	@tailwind base;
+	/* @tailwind components; */
+	@tailwind utilities;
+  </style>
